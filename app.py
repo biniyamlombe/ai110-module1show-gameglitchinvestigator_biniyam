6135d83eb5064a15b0e1cd6b33af9e40c92dad05
Biniyam Lombe <biniyamlombe@gmail.com>
2026-03-15 16:47:45 -0400

    Fix bug 2: Remove integer to string glitch in Check_Guess and attempt parsing

--- app.py
+++ app.py
@@ -33,17 +33,9 @@ def check_guess(guess, secret):
     if guess == secret:
         return "Win", "🎉 Correct!"
 
-    try:
-        if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
-        else:
-            return "Too Low", "📉 Go LOWER!"
-    except TypeError:
-        g = str(guess)
-        if g == secret:
-            return "Win", "🎉 Correct!"
-        if g > secret:
-            return "Too High", "📈 Go HIGHER!"
+    if guess > secret:
+        return "Too High", "📈 Go HIGHER!"
+    else:
         return "Too Low", "📉 Go LOWER!"
 
 
@@ -155,10 +147,7 @@ if submit:
     else:
         st.session_state.history.append(guess_int)
 
-        if st.session_state.attempts % 2 == 0:
-            secret = str(st.session_state.secret)
-        else:
-            secret = st.session_state.secret
+        secret = st.session_state.secret
 
         outcome, message = check_guess(guess_int, secret)
 
