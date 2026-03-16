88fc3761ae31c162c901627781c1320e1bde1108
Biniyam Lombe <biniyamlombe@gmail.com>
2026-03-15 23:52:20 -0400

    removed check_guess from app.py

--- app.py
+++ app.py
@@ -1,24 +1,6 @@
 import random
 import streamlit as st
-from logic_utils import get_range_for_difficulty, parse_guess
-
-
-def check_guess(guess, secret):
-    if guess == secret:
-        return "Win", "🎉 Correct!"
-
-    try:
-        if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
-        else:
-            return "Too Low", "📉 Go LOWER!"
-    except TypeError:
-        g = str(guess)
-        if g == secret:
-            return "Win", "🎉 Correct!"
-        if g > secret:
-            return "Too High", "📈 Go HIGHER!"
-        return "Too Low", "📉 Go LOWER!"
+from logic_utils import get_range_for_difficulty, parse_guess, check_guess
 
 
 def update_score(current_score: int, outcome: str, attempt_number: int):
@@ -129,10 +111,12 @@ if submit:
     else:
         st.session_state.history.append(guess_int)
 
-        if st.session_state.attempts % 2 == 0:
-            secret = str(st.session_state.secret)
-        else:
-            secret = st.session_state.secret
+        # if st.session_state.attempts % 2 == 0:
+        #     secret = str(st.session_state.secret)
+        # else:
+        #     secret = st.session_state.secret
+        secret = st.session_state.secret
+
 
         outcome, message = check_guess(guess_int, secret)
 
