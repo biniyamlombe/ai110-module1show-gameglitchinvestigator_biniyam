79814ae1ba74acef4e15d80ec1a9eeaa07e163b8
Biniyam Lombe <biniyamlombe@gmail.com>
2026-03-16 00:04:04 -0400

    Imported update_score

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 import streamlit as st
-from logic_utils import get_range_for_difficulty, parse_guess, check_guess
+from logic_utils import get_range_for_difficulty, parse_guess, check_guess, update_score
 
 
 st.set_page_config(page_title="Glitchy Guesser", page_icon="🎮")
